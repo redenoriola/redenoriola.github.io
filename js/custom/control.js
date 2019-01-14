a12d2000479678130b6ca1169e411c82eed11024
redenoriola <44425643+redenoriola@users.noreply.github.com>
2019-01-14 21:55:28 +0800

    Update control.js

--- js/custom/control.js
+++ js/custom/control.js
@@ -1,7 +1,25 @@
 //WoW Js
 new WOW().init();
-//
-
+//smooth sliding
+$('.slide-in').click(function (e) {
+    e.preventDefault();
+});
+$('.slide-in').on('click', function () {
+    $('.fade-in').toggleClass('show');
+});
+//Smooth Scrolling
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: (target.offset().top - 54)
+            }, 1000, "easeInOutExpo");
+            return false;
+        }
+    }
+});
 //Show Preloader while loading
 $(window).on('load', function () {
     $(".preloader").css("top", "100%");
