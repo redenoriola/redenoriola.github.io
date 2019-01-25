be7eb6d9206110bf04a12540d83034998c7deaef
redenoriola <44425643+redenoriola@users.noreply.github.com>
2019-01-25 21:31:09 +0800

    Final Update

--- js/custom/control.js
+++ js/custom/control.js
@@ -10,7 +10,7 @@ var laundrywash = [
     },
     {
         tag: 'Gallery',
-        details: 'This section displays pictures of the business. The purpose of this is to display what the laundry business is like as well as its works that are used to promote and attract potential customers.  '
+        details: 'This section displays pictures of the business. The purpose of this is to display what the laundry business is like as well as its products that are used to promote and attract potential customers.  '
     },
     {
         tag: 'Contact',
@@ -20,7 +20,7 @@ var laundrywash = [
 var sweetsunny = [
     {
         tag: 'Overview',
-        details: 'Sweet Sunny is a website that I made for my friend that is starting a leche flan business. This is to promote their business and products to potential customers. This website is static and has a fancy, stylish design that is suited for a business that sells sweets.'
+        details: 'Sweet Sunny is a website I made for my friend that is starting a leche flan business. This is to promote their business and products to potential customers. This website is static and has a fancy, stylish design that is suited for a sweets selling business.'
     },
     {
         tag: 'Products',
@@ -34,7 +34,7 @@ var sweetsunny = [
 var genesis = [
     {
         tag: 'Overview',
-        details: 'GENESIS: 2D Evolution Game is my thesis project when I was in 4th year college. This intends to increase the knowledge of players in terms of human evolution. The players are able to familiarize themselves with the evolution of human based on the book of Richard Dawkins Ancestors Tale.',
+        details: 'GENESIS: 2D Evolution Game is my thesis project when I was in 4th year college. This intends to increase the knowledge of players by means of a game. The players are able to familiarize themselves with the evolution of human based on the book of Richard Dawkins Ancestors Tale.',
     },
     {
         tag: 'In Game',
@@ -42,11 +42,11 @@ var genesis = [
     },
     {
         tag: 'Levels',
-        details: "The game has six levels in which the player must try his/her best in surviving. If the player fails to complete a level, he/she can retry until the level is completed. The character then evolves into a new and stronger organism that has greater chance of survival on the next level.",
+        details: "The game has six levels in which the player must try his/her best in surviving. If the player fails to complete a level, he/she can retry until the level is completed. The character then evolves into a new and stronger organism that has a greater chance of survival on the next level.",
     },
     {
         tag: 'Story',
-        details: "The game comes with a story. Everytime the player starts a level, a slideshow of images and texts are displayed and the player must read them before entering the game. This way the game becomes educational as well as fun and exciting.",
+        details: "The game comes with a story. Everytime the player starts a level, a slideshow of images and texts are displayed and the player must read them before entering the game. This way the game becomes educational and the players are having fun while learning.",
     },
     {
         tag: 'Objectives',
@@ -78,7 +78,7 @@ var dms = [
 var humbleshop = [
     {
         tag: "Overview",
-        details: "Humble Shop is a project I made when I was studying object-oriented programming of PHP and was the first ecommerce website I made. It was made similar functions to ecommerce websites like lazada that has a cart, check-out, order history and membership functionalities. ",
+        details: "Humble Shop is a project I made when I was studying object-oriented programming of PHP and was the first ecommerce website I made. It has similar functions to ecommerce websites like lazada that has a cart, check-out, order history and membership functions. ",
     },
     {
         tag: "Cart",
@@ -86,11 +86,11 @@ var humbleshop = [
     },
     {
         tag: "Checkout",
-        details: "This is the transaction page. This is where the customer choose a payment method to pay for all the items in the cart. The customer can choose to pay via cash on delivery or Paypal. ",
+        details: "This is the transactions page. This is where the customer chooses a payment method to pay for all the items in the cart. The customer can choose to pay via cash on delivery or Paypal. ",
     },
     {
         tag: "Orders",
-        details: "This is where the customer can see all of his/her orders check the time and date of its arrival. ",
+        details: "This is where the customer can see all of his/her orders and  check the time and date of its arrival. ",
     },
 ];
 var tribute = [
@@ -237,7 +237,7 @@ function findActive(x) {
             $(".modal-details .project-details").html(sweetsunny[active_id].details);
             break;
         case "3":
-            $(".modal-details .project-title").html("Genesis: A 2D Evolution Game");
+            $(".modal-details .project-title").html("Genesis: 2D Evolution Game");
             $(".modal-details .project-tag").html(genesis[active_id].tag);
             $(".modal-details .project-details").html(genesis[active_id].details);
             break;
@@ -255,6 +255,8 @@ function findActive(x) {
             $(".modal-details .project-title").html("Fifteen Martyrs of Bicol Tribute Page");
             $(".modal-details .project-tag").html(tribute[active_id].tag);
             $(".modal-details .project-details").html(tribute[active_id].details);
-            break;            
+            break;
     }
 }
+//Jquery Easing
+(function (factory) { if (typeof define === "function" && define.amd) { define(["jquery"], function ($) { return factory($) }) } else if (typeof module === "object" && typeof module.exports === "object") { exports = factory(require("jquery")) } else { factory(jQuery) } })(function ($) { $.easing.jswing = $.easing.swing; var pow = Math.pow, sqrt = Math.sqrt, sin = Math.sin, cos = Math.cos, PI = Math.PI, c1 = 1.70158, c2 = c1 * 1.525, c3 = c1 + 1, c4 = 2 * PI / 3, c5 = 2 * PI / 4.5; function bounceOut(x) { var n1 = 7.5625, d1 = 2.75; if (x < 1 / d1) { return n1 * x * x } else if (x < 2 / d1) { return n1 * (x -= 1.5 / d1) * x + .75 } else if (x < 2.5 / d1) { return n1 * (x -= 2.25 / d1) * x + .9375 } else { return n1 * (x -= 2.625 / d1) * x + .984375 } } $.extend($.easing, { def: "easeOutQuad", swing: function (x) { return $.easing[$.easing.def](x) }, easeInQuad: function (x) { return x * x }, easeOutQuad: function (x) { return 1 - (1 - x) * (1 - x) }, easeInOutQuad: function (x) { return x < .5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2 }, easeInCubic: function (x) { return x * x * x }, easeOutCubic: function (x) { return 1 - pow(1 - x, 3) }, easeInOutCubic: function (x) { return x < .5 ? 4 * x * x * x : 1 - pow(-2 * x + 2, 3) / 2 }, easeInQuart: function (x) { return x * x * x * x }, easeOutQuart: function (x) { return 1 - pow(1 - x, 4) }, easeInOutQuart: function (x) { return x < .5 ? 8 * x * x * x * x : 1 - pow(-2 * x + 2, 4) / 2 }, easeInQuint: function (x) { return x * x * x * x * x }, easeOutQuint: function (x) { return 1 - pow(1 - x, 5) }, easeInOutQuint: function (x) { return x < .5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2 }, easeInSine: function (x) { return 1 - cos(x * PI / 2) }, easeOutSine: function (x) { return sin(x * PI / 2) }, easeInOutSine: function (x) { return -(cos(PI * x) - 1) / 2 }, easeInExpo: function (x) { return x === 0 ? 0 : pow(2, 10 * x - 10) }, easeOutExpo: function (x) { return x === 1 ? 1 : 1 - pow(2, -10 * x) }, easeInOutExpo: function (x) { return x === 0 ? 0 : x === 1 ? 1 : x < .5 ? pow(2, 20 * x - 10) / 2 : (2 - pow(2, -20 * x + 10)) / 2 }, easeInCirc: function (x) { return 1 - sqrt(1 - pow(x, 2)) }, easeOutCirc: function (x) { return sqrt(1 - pow(x - 1, 2)) }, easeInOutCirc: function (x) { return x < .5 ? (1 - sqrt(1 - pow(2 * x, 2))) / 2 : (sqrt(1 - pow(-2 * x + 2, 2)) + 1) / 2 }, easeInElastic: function (x) { return x === 0 ? 0 : x === 1 ? 1 : -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * c4) }, easeOutElastic: function (x) { return x === 0 ? 0 : x === 1 ? 1 : pow(2, -10 * x) * sin((x * 10 - .75) * c4) + 1 }, easeInOutElastic: function (x) { return x === 0 ? 0 : x === 1 ? 1 : x < .5 ? -(pow(2, 20 * x - 10) * sin((20 * x - 11.125) * c5)) / 2 : pow(2, -20 * x + 10) * sin((20 * x - 11.125) * c5) / 2 + 1 }, easeInBack: function (x) { return c3 * x * x * x - c1 * x * x }, easeOutBack: function (x) { return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2) }, easeInOutBack: function (x) { return x < .5 ? pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2) / 2 : (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2 }, easeInBounce: function (x) { return 1 - bounceOut(1 - x) }, easeOutBounce: bounceOut, easeInOutBounce: function (x) { return x < .5 ? (1 - bounceOut(1 - 2 * x)) / 2 : (1 + bounceOut(2 * x - 1)) / 2 } }) });
